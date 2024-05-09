ab407842a4ee4218b42e65935c990b0a95efbd9b
Mattia Moffa <altermetax@gmail.com>
2024-05-09 02:18:34 +0200

    Add benchmarking system

--- autowordle/session_game_manager.py
+++ autowordle/session_game_manager.py
@@ -10,7 +10,7 @@ class SessionGameManager:
         self.next_id += 1
 
         if type == "wordle":
-            self.games[id] = Wordle(data_path, False, "WASTE")
+            self.games[id] = Wordle(data_path, False)
         elif type == "absurdle":
             self.games[id] = Absurdle(data_path)
 
---
+++ benchmark.py
@@ -0,0 +1,74 @@
+from autowordle.absurdle_solver import AbsurdleSolver
+from autowordle.dict_loader import get_possible_words
+from autowordle.wordle import Wordle
+
+# Run the provided solver on all possible Wordle solutions
+def run(data_path, solver_class):
+    solutions = get_possible_words(data_path)
+    attempt_counts = {}
+    
+    for solution in solutions:
+        results = test_solution(data_path, solver_class, solution)
+        if results["ok"]:
+            n_attempts = len(results["game"])
+            print(f"Solution {solution}: {n_attempts} attempts")
+            if n_attempts in attempt_counts:
+                attempt_counts[n_attempts] += 1
+            else:
+                attempt_counts[n_attempts] = 1
+            print(attempt_counts)
+        else:
+            print(f"Solution {solution}: {results['msg']}")
+        
+# Run the provided solver with the provided game on the provided solution
+def test_solution(data_path, solver_class, solution):
+    wordle = Wordle(data_path, False, solution)
+    solver = solver_class(data_path, verbose=False)
+
+    game = []
+    new_game_state = []
+    while True:
+        guess = solver.get_guess(new_game_state)["word"]
+        if guess is None:
+            return {
+                "ok": False,
+                "msg": "failed"
+            }
+        result = wordle.try_word(guess)
+        if not result["valid"]:
+            return {
+                "ok": False,
+                "msg": "used invalid word: {guess}"
+            }
+        
+        new_game_state = [[]]
+        for i in range(len(guess)):
+            new_game_state[0].append({"letter": guess[i], "color": result["result"][i]})
+        
+        game.append(new_game_state[0])
+        
+        green_count = 0
+        for c in result["result"]:
+            if c == "green":
+                green_count += 1
+        if green_count == 5:
+            break
+
+    return {
+        "ok": True,
+        "game": game
+    }
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        data_path = "."
+    elif len(sys.argv) == 2:
+        data_path = sys.argv[1]
+    else:
+        print(f"{sys.argv[0]}: please use either one or zero arguments.", file=sys.stderr)
+        print(f"Usage: {sys.argv[0]} [data_path]", file=sys.stderr)
+        print(f"where data_path is the path to a directory containing accepted_words.txt and possible_words.txt", file=sys.stderr)
+        sys.exit(1)
+
+    run(data_path, AbsurdleSolver)
